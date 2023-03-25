02615b689c9da301c7807f0ff23a34bfeca5a368
naizabekov12 <114017145+naizabekov12@users.noreply.github.com>
2023-03-25 18:27:07 +0600

    Add files via upload

---
+++ src/test/java/kg/ulan/SpringSecurityApp/SpringSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.ulan.SpringSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
